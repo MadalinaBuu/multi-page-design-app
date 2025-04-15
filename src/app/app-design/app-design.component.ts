1f130bf7347c7995b17df3db99fd6541cc947eb3
MadalinaBuu <madalina.ciucioiu@gmail.com>
2025-04-15 20:45:49 +0300

    added components + variables file

---
+++ src/app/app-design/app-design.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-design',
+  imports: [],
+  templateUrl: './app-design.component.html',
+  styleUrl: './app-design.component.scss'
+})
+export class AppDesignComponent {
+
+}
---
+++ src/app/web-design/web-design.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-design',
+  imports: [],
+  templateUrl: './web-design.component.html',
+  styleUrl: './web-design.component.scss'
+})
+export class WebDesignComponent {
+
+}
