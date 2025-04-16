44025b2e0f3932ea44c22b033d2565e38ebdd366
MadalinaBuu <madalina.ciucioiu@gmail.com>
2025-04-16 11:17:56 +0300

    added bootstrap + navbar wip

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavBarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
