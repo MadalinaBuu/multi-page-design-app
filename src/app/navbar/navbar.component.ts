742e819c99f93360d3b92fbeede1c7348c65f097
MadalinaBuu <madalina.ciucioiu@gmail.com>
2025-04-16 21:58:10 +0300

    navbar + assetes added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavBarComponent {
   isMenuOpen = false;
+
+  menuItems = [
+    { label: 'Our Company', link: '#' },
+    { label: 'Locations', link: '#' },
+    { label: 'Contact', link: '#' }
+  ];
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
