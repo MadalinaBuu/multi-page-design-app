e9dd458a51e7e845202c4ab78092a4927844d99c
MadalinaBuu <madalina.ciucioiu@gmail.com>
2025-04-18 11:15:26 +0300

    home - strengths-section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -39,4 +39,25 @@ export class HomeComponent {
       },
     },
   ];
+
+  strengths = [
+    {
+      name: "Passionate",
+      description: "Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.",
+      image: "../../assets/images/home/desktop/illustration-passionate.svg",
+      circleRotation: "rotate(0deg)" // No rotation for first image
+    },
+    {
+      name: "Resourceful",
+      description: "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.",
+      image: "../../assets/images/home/desktop/illustration-resourceful.svg",
+      circleRotation: "rotate(180deg)" // No rotation for first image
+    },
+    {
+      name: "Friendly",
+      description: "  We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide..",
+      image: "../../assets/images/home/desktop/illustration-friendly.svg",
+      circleRotation: "rotate(90deg)" // No rotation for first image
+    }
+  ];
 }
