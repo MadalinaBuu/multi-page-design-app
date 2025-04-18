d223c70df631fa79264930fc9d40cfc3aecbe20c
MadalinaBuu <madalina.ciucioiu@gmail.com>
2025-04-18 10:12:47 +0300

    home - service-section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LocationsComponent } from './locations/locations.component';
 import { ContactComponent } from './contact/contact.component';
+import { AppDesignComponent } from './app-design/app-design.component';
+import { WebDesignComponent } from './web-design/web-design.component';
+import { GraphicDesignComponent } from './graphic-design/graphic-design.component';
 
 export const routes: Routes = [
     { path: 'our-company', component: HomeComponent },
     { path: 'locations', component: LocationsComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'app-design', component: AppDesignComponent },
+    { path: 'graphic-design', component: GraphicDesignComponent },
+    { path: 'web-design', component: WebDesignComponent },
     { path: '**', redirectTo: 'our-company', pathMatch: 'full' } // Default route
   ];
